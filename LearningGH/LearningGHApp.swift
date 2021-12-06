8c7965ee4e7a1fb6c78d236c2034ffcf3f92a883
Kjell Espejord <espejord@online.no>
2021-12-06 16:47:52 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Kjell Espejord on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
