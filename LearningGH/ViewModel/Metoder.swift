38ef99b735d9abe5159cea1826f0369cc29105c8
Kjell Espejord <espejord@online.no>
2022-01-20 09:36:21 +0100

    setter inn func i en class

---
+++ LearningGH/ViewModel/Metoder.swift
@@ -0,0 +1,21 @@
+//
+//  Metoder.swift
+//  LearningGH
+//
+//  Created by Kjell Espejord on 20/01/2022.
+//
+
+import Foundation
+
+class NyeData: ObservableObject {
+
+    func finnData(first a:Int, andre b:Int) -> Int  {
+    
+    return a+b
+   
+        
+}
+  
+}
+         
+
